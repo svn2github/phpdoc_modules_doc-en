1da935936ad8093dbf3a1c61422a194a2c2f9daf
bjori <bjori@c90b9560-bf6c-de11-be94-00142212c4b1>
2010-03-21 15:15:18 +0000

    - PhD is no longer beta - You'll need to PHP package too - --enable-xml-details is an argument to configure.php, not PhD
    git-svn-id: http://svn.php.net/repository/phpdoc/modules/doc-en@296477 c90b9560-bf6c-de11-be94-00142212c4b1

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 # svn co http://svn.php.net/repository/phpdoc/modules/doc-en
-# pear install doc.php.net/phd-beta
+# pear install doc.php.net/phd
+# pear install doc.php.net/phd_php
 # cd doc-en
 
-php doc-base/configure.php
-phd --docbook doc-base/.manual.xml --package PHP --format xhtml --enable-xml-details
+php doc-base/configure.php  --enable-xml-details
+phd --docbook doc-base/.manual.xml --package PHP --format xhtml
 open output/php-chunked-xhtml/index.html &
+
